f3d1080a909437bda56d0818cc5aed5e682bc0b5
galenomoon <galeno.dev@gmail.com>
2023-06-16 21:30:52 -0300

    fix parseint to float

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,19 +21,30 @@ export default function Home() {
         <title>Calculadora de Notas</title>
         <meta name="description" content="Calculadora de Notas" />
       </Head>
-      <div className='flex gap-2'>
-        <input
-          value={N1}
-          type='number'
-          className='border-[3px] outline-none border-[#E8A0BF] w-[100px] p-2 text-center bg-[#F8E8EE] rounded-full text-2xl'
-          onChange={(e) => setN1(parseInt(e.target.value))}
-        />
-        <input
-          value={N2}
-          type='number'
-          className='border-[3px] outline-none border-[#E8A0BF] w-[100px] p-2 text-center bg-[#F8E8EE] rounded-full text-2xl'
-          onChange={(e) => setN2(parseInt(e.target.value))}
-        />
+      <div className='flex gap-9'>
+        <div className='flex items-center gap-2 justify-center'>
+          <p>
+            N1:
+          </p>
+
+          <input
+            value={N1}
+            type='number'
+            className='border-[3px] outline-none border-[#E8A0BF] w-[100px] p-2 text-center bg-[#F8E8EE] rounded-full text-2xl'
+            onChange={(e) => setN1(parseFloat(e.target.value))}
+          />
+        </div>
+        <div className='flex items-center gap-2 justify-center'>
+          <p>
+            N2:
+          </p>
+          <input
+            value={N2}
+            type='number'
+            className='border-[3px] outline-none border-[#E8A0BF] w-[100px] p-2 text-center bg-[#F8E8EE] rounded-full text-2xl'
+            onChange={(e) => setN2(parseFloat(e.target.value))}
+          />
+        </div>
       </div>
       <p className='mt-6 text-6xl'>
         Nota Final {final}
